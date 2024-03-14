2227797860bfb05b79ee71130f0d1f5cccdde987
jpmoraes06 <150357838+jpmoraes06@users.noreply.github.com>
2024-03-14 14:24:47 -0300

    Add files via upload

---
+++ MapDiagrama1/src/main/java/com/example/demo/entities/Venda.java
@@ -0,0 +1,37 @@
+package com.example.demo.entities;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "venda")
+public class Venda {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
+	private Date data_venda;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente;
+
+}
+
+
